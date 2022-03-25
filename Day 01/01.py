d52c616207c70c11493485695836549e6f99c258
mukesh <mukeshamra@hotmail.com>
2022-03-24 21:32:21 -0400

    Added Two file and modified the indemo.txt

---
+++ Day 01/01.py
@@ -0,0 +1,77 @@
+# # Texts --- > Strings
+
+
+# # "it's"
+
+# # print(hello)
+
+# # x = 3
+# # y = 2
+
+# # message = '''Hello
+# # John, Its a beautiful day today!'''
+
+# # print(message)
+
+# # print(message)
+# # print(message)
+
+
+# greeting = 'Hello'
+# name = 'John'
+
+# # message = greeting + ', ' + name +'!'
+# # message = '{}, {}'.format(greeting, name)
+# # message = f"""{greeting}, {name}"""
+
+# print(message)
+
+# Textual & Numbers
+
+"""
+Numbers --> Whole Numbers -- Integers
+			Decimals -- Floats
+
+"""
+
+# num = 3.14
+
+# print(type(num))
+
+"""
++
+-
+*
+/
+//
+**
+%
+"""
+
+var_1 = 3
+var_2 = 2
+
+# result = var_1  var_2
+# print(f"The output is : {result}")
+
+# message = 'hello'
+# message = 'Hi'
+
+# print(message)
+
+# num = 1
+
+# # num = num + 1
+# num += 1
+
+# print(num)
+
+num_1 = 100
+num_2 = 200
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
+
+
